94698e62e50f7e8e97a6f7983888e550b3328b63
oluchidev <jennypinkoc@gmaiil.com>
2023-01-16 12:39:25 +0100

    Updates

---
+++ precache-manifest.a2263a4224d9423a55af867c982fa6a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfa93491be9dd27fb132",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/css/main.36e9816b.chunk.css"
+  },
+  {
+    "revision": "bfa93491be9dd27fb132",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/js/main.bfa93491.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "6c10488c2321fe7f4620",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/css/2.79991f9e.chunk.css"
+  },
+  {
+    "revision": "6c10488c2321fe7f4620",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/js/2.6c10488c.chunk.js"
+  },
+  {
+    "revision": "59e3cb6f350afcf8c139",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/js/runtime~main.59e3cb6f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "49d344770d5d60e8a573d781027519c8",
+    "url": "%7Bhttps://github.com/oluchidev%20%7D.github.io/%7Bhttps://github.com/oluchidev/Vintageberets%7D/index.html"
+  }
+];
